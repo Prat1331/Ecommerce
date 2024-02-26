a4554cd384be8307c3ed7991656d9416cf1a4f5b
skillbasket <shivanisharma232005@gmail.com>
2024-02-27 00:11:58 +0530

    hi

--- ec/store/urls.py
+++ ec/store/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path("address/", views.address, name="address"),
     path('UpdateAddress/<int:pk>', views.updateAddress.as_view(), name='updateAddress'),
 
-    # path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
+    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
     # path('cart/', views.show_cart, name='showcart'),
    
    #login authentication
--- ec/store/views.py
+++ ec/store/views.py
@@ -92,5 +92,12 @@ class updateAddress(View):
         return redirect("address")
     
 def add_to_cart(request):
-    pass    
+    user=request.user
+    product_id=request.GET.get('prod_id')    
+    product = Product.objects.get(id=product_id)
+    Cart(user=user,product=product).save()
           
+def show_cart(request):
+    user = request.user
+    cart = Cart.objects.filter(user=user)
+    return render(request, 'app/addtocart.html',locals())          
