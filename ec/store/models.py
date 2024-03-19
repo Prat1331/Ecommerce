be8d025e326d373653f0b7d63fb37ec45fa5e093
skillbasket <shivanisharma232005@gmail.com>
2024-03-19 09:43:09 +0530

    hii

--- ec/store/models.py
+++ ec/store/models.py
@@ -97,17 +97,17 @@ STATUS_CHOICES=(
 )
 
 class Payment(models.Model):
-    user = models.Foreignkey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     amount = models.FloatField()
     razorpay_order_id = models.CharField(max_length=100, blank=True, null=True) 
-    razorpay_payment_status = models.CharField(max_length=100, blank=True, mull=True) 
+    razorpay_payment_status = models.CharField(max_length=100, blank=True, null=True) 
     razorpay_payment_id= models. CharField(max_length=100, blank=True, null=True) 
     paid = models.BooleanField(default=False)        
 
 class OrderPlaced(models.Model):
-    user = models.Foreignkey(User,on_delete=models.CASCADE)
-    customer = models.Foreignkey(Customer,on_delete=models.CASCADE) 
-    product = models.Foreignkey(Product,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE) 
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity = models. PositiveIntegerField(default=1)
     ordered_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="Pending")
